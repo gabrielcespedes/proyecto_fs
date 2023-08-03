0e74c42e93525a0020a984c5c9f25991c66a01c9
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-08-03 17:32:59 -0400

    funciona POST artwork  falta detalle retro

--- Frontend/src/services/artworksService.js
+++ Frontend/src/services/artworksService.js
@@ -10,3 +10,18 @@ export const getArtworks = async () => {
         throw error;
     }
 };
+
+export const addArtwork = async (artwork, token) => {
+    try {
+        const response = await axios.post('http://localhost:3000/artworks', artwork, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        }); 
+        return response.data;            
+    } catch(error) {
+        console.error('Error al agregar el producto: ', error);
+        throw error;
+    }
+};
--- Frontend/src/views/UserViewBuyer.jsx
+++ Frontend/src/views/UserViewBuyer.jsx
@@ -6,40 +6,79 @@ import CardGroup from 'react-bootstrap/CardGroup'
 import { useContext } from "react";
 import MyContext from "../my_context";
 
+import { addArtwork } from '../services/artworksService';
+
 const UserViewBuyer = ({ user }) => {
+
+    const [artworkData, setArtworkData] = useState({
+        title: '',
+        description: '',
+        price: 0,
+        url_image: '',        
+    });    
+    
+    const token = localStorage.getItem('token');
+    
+    const handleAddArtwork = async () => {
+        try {
+            const artworkWithSeller = {...artworkData, seller_id: user.user_id};
+            const addedArtwork = await addArtwork(artworkWithSeller, token);
+            setArtworks([...artworks, addedArtwork ]);
+        } catch (error) {
+            //Manejo Errores
+        }
+    }
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+
+        let newValue;
+
+        if (name === 'price') {
+            newValue = !isNaN(Number(value)) ? Number(value) : 0;
+        } else {
+            newValue = value;
+        }
+
+        setArtworkData((prevData) => ({
+            ...prevData,
+            [name]: newValue,
+        }));        
+    };
+
     const { artistsInfo, artworks, setArtworks } = useContext(MyContext);
     const artistCollection = artworks.filter((element) => element.seller_id == user.user_id);
 
-    const [input_title, setInput_title] = useState('');
-    const [input_description, setInput_description] = useState('');
-    const [input_price, setInput_price] = useState(0);
-    const [input_url_image, setInput_url_image] = useState('');
+    // const [input_title, setInput_title] = useState('');
+    // const [input_description, setInput_description] = useState('');
+    // const [input_price, setInput_price] = useState(0);
+    // const [input_url_image, setInput_url_image] = useState('');
 
-    const handleInput_title = (e) => {
-        setInput_title(e.target.value);
-    }
-    const handleInput_description = (e) => {
-        setInput_description(e.target.value);
-    }
-    const handleInput_price = (e) => {
-        const value = Number(e.target.value);
-        if (!isNaN(value)) {
-            setInput_price(value);
-        } else {
-            setInput_price(0);
-        }        
-    }
-    const handleInput_url_image = (e) => {
-        setInput_url_image(e.target.value);
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setArtworks([...artworks, {product_id: artworks.length + 1, title: input_title, description: input_description, price: input_price, url_image: input_url_image, seller_id: user.user_id, amount: 0}])
-        setInput_title('');
-        setInput_description('');
-        setInput_price(0);
-        setInput_url_image('');
-    }
+    // const handleInput_title = (e) => {
+    //     setInput_title(e.target.value);
+    // }
+    // const handleInput_description = (e) => {
+    //     setInput_description(e.target.value);
+    // }
+    // const handleInput_price = (e) => {
+    //     const value = Number(e.target.value);
+    //     if (!isNaN(value)) {
+    //         setInput_price(value);
+    //     } else {
+    //         setInput_price(0);
+    //     }        
+    // }
+    // const handleInput_url_image = (e) => {
+    //     setInput_url_image(e.target.value);
+    // }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     setArtworks([...artworks, {product_id: artworks.length + 1, title: input_title, description: input_description, price: input_price, url_image: input_url_image, seller_id: user.user_id, amount: 0}])
+    //     setInput_title('');
+    //     setInput_description('');
+    //     setInput_price(0);
+    //     setInput_url_image('');
+    // }
 
     console.log('Datos de usuario:', user);
     return (
@@ -66,26 +105,20 @@ const UserViewBuyer = ({ user }) => {
                 </CardGroup>
                 <Card className='mt-5 mb-5 text-center'>
                     <h2>Agregar Obra</h2>
-                    <form onSubmit={handleSubmit}>
-                        <input className='form-control' placeholder='Ingresa el nombre de la obra' onChange={handleInput_title} value={input_title}></input>
+                    <div className='form-group'>
+                        <input className='form-control'  type='text' name='title' value={artworkData.title}  onChange={handleInputChange} placeholder='Ingresa el nombre de la obra' ></input>
                         <br></br>
-                        <input className='form-control' placeholder='Descripción' onChange={handleInput_description} value={input_description}></input>
+                        <input className='form-control' type='text' name='description' value={artworkData.description}  onChange={handleInputChange} placeholder='Descripción' ></input>
                         <br></br>
                         <label className='label-left'>Valor:</label>
-                        <input className='form-control' placeholder='Valor' onChange={handleInput_price} value={input_price}></input>
+                        <input className='form-control' type='number' name='price' value={artworkData.price}  onChange={handleInputChange} placeholder='Valor'></input>
                         <br></br>
-                        <input className='form-control' placeholder='url imagen' onChange={handleInput_url_image} value={input_url_image}></input>
+                        <input className='form-control' type='text' name='url_image' value={artworkData.url_image}   onChange={handleInputChange} placeholder='url imagen' ></input>
                         <br></br>
-                        <button className='btn btn-dark mb-2'>Agregar obra</button>
-                    </form>
+                        <button onClick={handleAddArtwork} className='btn btn-dark mb-2'>Agregar obra</button>
+                    </div>
                     {console.log(artworks)}
-                </Card>
-                {/* Para implementarlo a futuro*/}
-                {/* <Row>
-                <Col xs={12} md={12}>
-                    <UserCard user={user} />
-                </Col>
-                </Row> */}
+                </Card>                
             </Container>            
         </Container>
     );
